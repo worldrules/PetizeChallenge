d5400d57d28ff0cb0a6d8841b3f6b3f0a9cdfcba
worldrulesdev <worldrulesdev@gmail.com>
2023-08-23 16:56:11 -0300

    Ajuste na service

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class GithubService {
+export class UserService {
+  constructor(private http: HttpClient) { }
+
+  getUser(name: string): Observable<User> {
+    const url = `https://api.github.com/users/${name}`;
+    return this.http.get<User>(url);
+  }
 }
