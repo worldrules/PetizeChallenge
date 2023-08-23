b70730af88c536e1d0cc02a834f4554a379c9467
worldrulesdev <worldrulesdev@gmail.com>
2023-08-23 17:24:57 -0300

    Config and pages init

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NotFoundComponent {
 
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
-import { GithubService } from './../../services/github.service';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { User } from '../../models/user.model';
 @Component({
-  selector: 'app-profile',
+  selector: 'app-user',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProfileComponent {
+export class UserComponent implements OnInit {
+
+  constructor() { }
+  @Input()
+  user: User;
+  ngOnInit() {
+  }
 
 }
