44c72777a97679e4da3545b0a27a076e4722fad2
worldrulesdev <worldrulesdev@gmail.com>
2023-08-24 23:51:57 -0300

    Criando testes unitarios isolados

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,21 +1,69 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of, throwError } from 'rxjs';
+import { RouterModule } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home.component';
+import { GithubService } from '../../services/github.service';
+import { User } from '../../models/user.model';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let githubService: GithubService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HomeComponent],
+      imports: [ReactiveFormsModule, HttpClientTestingModule, HttpClientModule, RouterModule],
+      providers: [GithubService,]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HomeComponent]
-    });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    githubService = TestBed.inject(GithubService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set user and error properties on successful search', fakeAsync(() => {
+    const mockUser: User = {
+      name: 'leonardo',
+      login: 'worldrules',
+      avatar_url: "https://avatars.githubusercontent.com/u/33266800?v=4",
+      created_at: "2017-10-31T20:23:23Z",
+      location: 'RS',
+      bio: "Cartomante - Ex Jogador de LOL - O melhor jogador de UNO",
+      public_repos: 104,
+      public_gists: 15,
+      followers: 5,
+      html_url: ''
+    };
+    spyOn(githubService, 'getUser').and.returnValue(of(mockUser));
+
+    component.findControl.setValue('username-to-search');
+    tick(1000);
+    fixture.detectChanges();
+
+    expect(component.user).toBe(mockUser);
+    expect(component.error).toBeFalse();
+  }));
+
+  it('should handle error on failed search', fakeAsync(() => {
+    spyOn(githubService, 'getUser').and.returnValue(throwError('some error'));
+
+    component.findControl.setValue('invalid-username');
+    tick(1000);
+    fixture.detectChanges();
+
+    expect(component.user).toBeNull();
+    expect(component.error).toBeTrue();
+  }));
 });
--- src/app/components/perfil-stars/perfil-stars.component.spec.ts
+++ src/app/components/perfil-stars/perfil-stars.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
 import { PerfilStarsComponent } from './perfil-stars.component';
 
@@ -6,10 +9,14 @@ describe('PerfilStarsComponent', () => {
   let component: PerfilStarsComponent;
   let fixture: ComponentFixture<PerfilStarsComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PerfilStarsComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [PerfilStarsComponent]
-    });
     fixture = TestBed.createComponent(PerfilStarsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +24,27 @@ describe('PerfilStarsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should search repositories and sort them by stargazers_count', () => {
+    const mockRepos = [
+      { name: 'Repo1', description: 'Description 1', stargazers_count: 10, html_url: 'url1' },
+      { name: 'Repo2', description: 'Description 2', stargazers_count: 5, html_url: 'url2' },
+    ];
+
+    spyOn(component['http'], 'get').and.returnValue(of(mockRepos));
+
+    component.searchQuery = 'some-username';
+    component.searchRepositories();
+
+    expect(component.repositories).toEqual(mockRepos);
+  });
+
+  it('should navigate back', () => {
+    const routerSpy = spyOn(component['router'], 'navigate');
+
+    component.goBack();
+
+    expect(routerSpy).toHaveBeenCalledWith(['/']);
   });
 });
