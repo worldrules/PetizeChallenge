4761a4c115364ada09f5b0dbc37d7aaeb6bd55bc
worldrulesdev <worldrulesdev@gmail.com>
2023-08-24 23:10:34 -0300

    iniciando busca por repositorios e ordem por estrela

--- src/app/components/perfil-stars/perfil-stars.component.ts
+++ src/app/components/perfil-stars/perfil-stars.component.ts
@@ -1,24 +1,38 @@
 import { GithubService } from './../../services/github.service';
 import { Component, Input, OnInit } from '@angular/core'
 import { User } from 'src/app/models/user.model';
+import { HttpClient } from '@angular/common/http';
+
+interface Repository {
+  name: string;
+  description: string;
+  stargazers_count: number;
+  html_url: string;
+}
+
 
 @Component({
   selector: 'app-perfil-stars',
   templateUrl: './perfil-stars.component.html',
   styleUrls: ['./perfil-stars.component.css']
 })
-export class PerfilStarsComponent implements OnInit {
-  constructor(private githubService: GithubService) { }
+export class PerfilStarsComponent {
+  constructor(private http: HttpClient) { }
   @Input()
   user: User;
   sortedRepos: any[];
+  searchQuery: string = '';
+  repositories: Repository[] = [];
 
-  ngOnInit() {
+  searchRepositories() {
+    if (this.searchQuery.trim() !== '') {
+      const apiUrl = `https://api.github.com/users/${this.searchQuery}/repos`;
 
-    this.githubService.getUserRepos(this.user.name)
-    this.githubService.getUserRepos(this.user.name).subscribe((sortedRepos) => {
-      this.sortedRepos = sortedRepos.sort((a, b) => b.stargazers_count - a.stargazers_count);
-    });
+      this.http.get<Repository[]>(apiUrl)
+        .subscribe(data => {
+          this.repositories = data.sort((a, b) => b.stargazers_count - a.stargazers_count);
+        });
+    }
   }
 
 }
